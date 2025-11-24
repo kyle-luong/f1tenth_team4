a2bb1e3b6e69c9fa5f60c34012f15ac1a2398220
Christopher Wood <woodchristophermichael@gmail.com>
2025-11-24 01:28:56 -0500

    ok we got some pursuing

--- f1tenth_purepursuit/src/pure_pursuit.py
+++ f1tenth_purepursuit/src/pure_pursuit.py
@@ -3,6 +3,7 @@
 # Import necessary libraries
 import rospy
 import os
+import numpy as np
 import sys
 import csv
 import math
@@ -16,11 +17,13 @@ import tf
 plan                = []
 path_resolution     = []
 frame_id            = 'map'
-car_name            = str(sys.argv[1])
-trajectory_name     = str(sys.argv[2])
+# car_name            = str(sys.argv[1])
+# trajectory_name     = str(sys.argv[2])
+car_name            = 'car_4'
+trajectory_name     = 'raceline'
 
 # Publishers for sending driving commands and visualizing the control polygon
-command_pub         = rospy.Publisher('/{}/offboard/command'.format(car_name), AckermannDrive, queue_size = 1)
+command_pub         = rospy.Publisher('/{}/offboard/command'.format(car_name), AckermannDrive, queue_size = 10)
 polygon_pub         = rospy.Publisher('/{}/purepursuit_control/visualize'.format(car_name), PolygonStamped, queue_size = 1)
 
 # Global variables for waypoint sequence and current polygon
@@ -30,10 +33,16 @@ global curr_polygon
 wp_seq          = 0
 control_polygon = PolygonStamped()
 
+def _get_distance(point1, point2):
+    # eucliodian distance
+    x1, y1 = point1[0], point1[1]
+    x2, y2 = point2[0], point2[1]
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 def construct_path():
     # Function to construct the path from a CSV file
     # TODO: Modify this path to match the folder where the csv file containing the path is located.
-    file_path = os.path.expanduser('/home/maxwell/catkin_ws/src/f1tenth_purepursuit/path/{}.csv'.format(trajectory_name))
+    file_path = os.path.expanduser('~/catkin_ws/src/f1tenth_purepursuit/path/{}.csv'.format(trajectory_name))
     with open(file_path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         for waypoint in csv_reader:
@@ -44,10 +53,12 @@ def construct_path():
         for point in range(0, len(plan[index])):
             plan[index][point] = float(plan[index][point])
 
+
     for index in range(1, len(plan)):
          dx = plan[index][0] - plan[index-1][0]
          dy = plan[index][1] - plan[index-1][1]
          path_resolution.append(math.sqrt(dx*dx + dy*dy))
+
 
 
 # Steering Range from -100.0 to 100.0
@@ -75,7 +86,21 @@ def purepursuit_control_node(data):
     # The base projection is defined as the closest point on the reference path to the car's current position.
     # Calculate the index and position of this base projection on the reference path.
     
+    odom = [odom_x, odom_y]
     # Your code here
+    def get_base_projection():
+        min_dist = 999
+        min_idx = 0 
+        # min_pt = (0, 0)
+        for idx, pt in enumerate(plan):
+            distance = _get_distance(odom, pt)
+            if distance < min_dist :
+                min_dist = distance
+                min_idx = idx
+                # min_pt = (pt[0], pt[1]) # x,y
+        return min_idx
+    
+    min_idx = get_base_projection()
 
     
     # Calculate heading angle of the car (in radians)
@@ -86,7 +111,7 @@ def purepursuit_control_node(data):
     
 
     # TODO 2: You need to tune the value of the lookahead_distance
-    lookahead_distance = 1.0
+    lookahead_distance = 2.0
 
 
     # TODO 3: Utilizing the base projection found in TODO 1, your next task is to identify the goal or target point for the car.
@@ -95,18 +120,91 @@ def purepursuit_control_node(data):
     # Calculate the position of this goal/target point along the path.
 
     # Your code here
+    def get_target_point():
+        idx = min_idx
+        while idx < len(plan) - 1:
+            distance = _get_distance(odom, plan[idx])
+            if distance >= lookahead_distance:
+                break
+            idx += 1
+        x = plan[idx - 1][0]
+        y = plan[idx - 1][1]
+        target_pt = (x, y)
+        # we could make this interpolate, so we dont have points behind corners
+        # in class it was discussed that we could also just have more waypoints to prevent that issue
+        return target_pt
+    
+    target_pt = get_target_point()
 
+    # print("DATA: ")
+    # print(data)
+    # print(target_pt)
+    # print(odom)
 
     # TODO 4: Implement the pure pursuit algorithm to compute the steering angle given the pose of the car, target point, and lookahead distance.
     # Your code here
+    def calculate_steering():
+        # alpha = arcsin(y_t / l_d)
+        # delta = arctan(2L * sin(alpha) / l_d)
+
+        # Basically, the pure pursuit angle calcs expects y_t to be in 
+        # the robot's frame, so we have to find the delta position
+        # from the center of the car to the target point then
+        # do an anti-rotation by the car's pose
+        carFrameTarget = tf.transformations.quaternion_from_euler(0, 0, 0)
+        carFrameTarget[0] = target_pt[0] - odom[0]
+        carFrameTarget[1] = target_pt[1] - odom[1]
+        carFrameTarget[2] = 0
+        carFrameTarget[3] = 0
+        orientation = tf.transformations.quaternion_from_euler(0, 0, 0)
+        orientation[0] = data.pose.orientation.x
+        orientation[1] = data.pose.orientation.y
+        orientation[2] = data.pose.orientation.z
+        orientation[3] = data.pose.orientation.w
+        conj = tf.transformations.quaternion_conjugate(orientation)
+        carFrameTarget = tf.transformations.quaternion_multiply(conj, carFrameTarget)
+        carFrameTarget = tf.transformations.quaternion_multiply(carFrameTarget, orientation)
+
+        alpha = math.asin((carFrameTarget[1]) / lookahead_distance)
+        # print("Angle: ", math.degrees(alpha))
+        steering_angle = math.atan(2 * WHEELBASE_LEN * math.sin(alpha) / lookahead_distance)
+        steering_angle = math.degrees(steering_angle)
+        return steering_angle
 
 
     # TODO 5: Ensure that the calculated steering angle is within the STEERING_RANGE and assign it to command.steering_angle
     # Your code here    
-    command.steering_angle = 0.0
+    command.steering_angle = 2.5 * calculate_steering()
+    # print("Steering at: ", command.steering_angle)
 
     # TODO 6: Implement Dynamic Velocity Scaling instead of a constant speed
-    command.speed = 20.0
+    def calculate_velocity(angle):
+        angle = abs(angle)
+        # dynamic velocity code from ftg
+        min_vel = 30.0
+        max_vel = 60.0
+
+        angleMax = 50.0
+
+        if angle > angleMax:
+            return min_vel
+        # dist = _get_distance(odom, target_pt)
+
+        # if the target is further than 3 meters, drive the max speed
+        # if dist > 3:
+        #     return max_vel
+        #
+        # # if the gap is really close, drive the min velocity
+        # if dist < 0.5:
+        #     return min_vel
+        #
+        # else, scale based on the distance
+        velocity = (angleMax - angle) / angleMax * (max_vel - min_vel) + min_vel
+        # velocity = max(min_vel, min(max_vel, velocity))
+        return velocity
+
+    command.speed = calculate_velocity(command.steering_angle)
+    # command.speed = 30.0
     command_pub.publish(command)
 
     # Visualization code
@@ -116,10 +214,10 @@ def purepursuit_control_node(data):
     # - target_x, target_y: Position of the goal/target point
 
     # These are set to zero only so that the template code builds. 
-    pose_x=0    
-    pose_y=0
-    target_x=0
-    target_y=0
+    pose_x=odom_x    
+    pose_y=odom_y
+    target_x=target_pt[0]
+    target_y=target_pt[1]
 
 
     base_link    = Point32()
@@ -146,6 +244,7 @@ if __name__ == '__main__':
         if not plan:
             rospy.loginfo('obtaining trajectory')
             construct_path()
+            rospy.loginfo('trajectory done!')
 
         # This node subsribes to the pose estimate provided by the Particle Filter. 
         # The message type of that pose message is PoseStamped which belongs to the geometry_msgs ROS package.
@@ -155,3 +254,5 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
 
         pass
+
+
--- race/src/ftg_control.py
+++ race/src/ftg_control.py
@@ -1,39 +1,239 @@
 #!/usr/bin/env python
 import math
 import rospy
+import numpy as np
 from race.msg import pid_input
+from sensor_msgs.msg import LaserScan
+from tf.transformations import quaternion_from_euler
 from ackermann_msgs.msg import AckermannDrive
+from visualization_msgs.msg import Marker, MarkerArray
+from geometry_msgs.msg import PolygonStamped, Point32, Point
 from std_msgs.msg import Float32
 
+class FTGController:
+	def __init__(self):
+		self.min_vel = 10.0
+		self.max_vel = 30.0
+		self.steering_gain = 2
+		self.min_gap_threshold = 2
+		self.angle_min = 0
+		self.angle_max = 0
+		self.angle_increment = 0
+		self.steering_alpha = 0.3
+		self.depth_threshold = 3
 
-servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
-prev_error = 0.0
+		self.drive_pub = rospy.Publisher('/car_4/multiplexer/command', AckermannDrive, queue_size = 10)
+
+		self.targetPoint = rospy.Publisher("/sphere_marker", Marker, queue_size = 20)
+		self.targets = rospy.Publisher("/target_scan", LaserScan, queue_size = 20)
+		self.footprint_pub = rospy.Publisher('/visualization/flootprint', PolygonStamped, queue_size=20)
+		self.steering_pub = rospy.Publisher('/visualization/steering', Marker, queue_size=20)
+		self.footprint_pub = rospy.Publisher('/visualization/disparities', MarkerArray, queue_size=1)
 
 
-# Publisher for moving the car.
-# TODO: Use the coorect topic /car_x/offboard/command. The multiplexer listens to this topic
-command_pub = rospy.Publisher('/car_4/multiplexer/command', AckermannDrive, queue_size = 10)
-
-def control(data):
-	global prev_error
-	global vel_max
-	global kp
-	global kd
-	global angle
-	global max_error
+		rospy.Subscriber('/disparity_scan', LaserScan, self.scan_callback)
 
 	
-	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
-	command = AckermannDrive()
-	command.steering_angle = 0
-	command.speed = 0	
+	def preprocess_lidar(self, ranges, scan):
+		# Filter to front view (-90 to +90) only
+		ranges = np.array(ranges)
+		angles = scan.angle_min + np.arange(len(ranges)) * scan.angle_increment
+		angles_deg = np.degrees(angles)
 
+		self.angle_increment = scan.angle_increment
+		self.angle_min = scan.angle_min
+		self.angle_max = scan.angle_max
+		
+		mask = (angles_deg >= -90) & (angles_deg <= 90)
+		front_ranges = ranges.copy()
+		
+		# Zero out points outside front view and invalid readings
+		front_ranges[~mask] = 0.0
+		front_ranges[~np.isfinite(front_ranges)] = 0.0
+		
+		return front_ranges.tolist()
 
-	# Move the car autonomously
-	command_pub.publish(command)
+	def scan_callback(self, scan_msg):
+		# scan and publish drive cmd
+		# not too sure rnscan_msg
+		processed_ranges = self.preprocess_lidar(scan_msg.ranges, scan_msg)
+		gap_idx, gap_dist = self.find_gap(processed_ranges)
+
+		if gap_dist >= self.depth_threshold:
+			start, end = self.find_max_gap(processed_ranges)
+			if end < start:
+				gap_idx, gap_dist = self.find_gap(processed_ranges)
+			else:
+				gap_idx = (start + end) // 2
+				gap_idx = max(0, min(len(processed_ranges)-1, gap_idx))
+				gap_dist = processed_ranges[gap_idx]
+		else:
+			gap_idx, gap_dist = self.find_gap(processed_ranges)
+
+		steering_angle = self.calculate_steering(gap_idx, len(processed_ranges))
+		velocity = self.calculate_velocity(gap_dist)
+		# cornering_steering_angle = self.cornering(processed_ranges, steering_angle,scan_msg.angle_min, scan_msg.angle_increment)
+		self.visualize(scan_msg, processed_ranges, gap_idx, gap_dist)
+		self.publish_drive(steering_angle, velocity)
+
+	# for wiggly problems
+	def find_max_gap(self, ranges):
+		max_run = 0
+		start_idx = 0
+		end_idx = 0
+		current_run = 0
+		current_start = 0
+
+		for i, r in enumerate(ranges):
+			if r > self.min_gap_threshold:
+				if current_run == 0:
+					current_start = i
+				current_run += 1
+			else:
+				if current_run > max_run:
+					max_run = current_run
+					start_idx = current_start
+					end_idx = i - 1
+				current_run = 0
+
+		if current_run > max_run:
+			max_run = current_run
+			start_idx = current_start
+			end_idx = len(ranges) - 1
+
+		return start_idx, end_idx
+
+	def find_gap(self, ranges):
+		# bc of extender we can greedily pick the furthest point
+		if max(ranges) == 0:
+			return len(ranges)//2, 0.0
+		gap_idx = ranges.index(max(ranges))
+		gap_dist = max(ranges)
+		return gap_idx, gap_dist
+	
+
+	def calculate_steering(self, gap_idx, total_len):
+		size = total_len - 1
+
+		# angle = gap_idx / len(ranges) * (scan.angle_max - scan.angle_min) + scan.angle_min
+		# angle *= 180 / math.pi 
+		# angle = math.degrees(gap_idx / float(len(ranges)) * (scan.angle_max - scan.angle_min) + scan.angle_min)
+
+		# delta = gap_idx - size/2.0 
+		# angle = delta/(size/2.0) * 90
+
+		# angle *= -1
+		# angle = max(-100, min(100, angle))
+		# print('steering angle', angle)
+
+		angle = self.angle_min + gap_idx * self.angle_increment
+		raw_steer = -1 * self.steering_gain * math.degrees(angle)
+		return raw_steer
+
+		print("steering angle:", angle)
+
+        # for smooth steering
+		# alpha = float(self.steering_alpha)
+		# smoothed = alpha * self.prev_steering + (1.0 - alpha) * raw_steer
+		# self.prev_steering = smoothed
+		# return smoothed
+
+	def cornering(self, ranges, desired_angle, angle_min, angle_increment):
+
+		distance = 0.2
+		min_turn_angle = 10
+		left_clear = True
+		right_clear = True
+
+		desired_angle_deg = math.degrees(desired_angle)
+
+		for i in range(len(ranges)):
+			angle = angle_min + i * angle_increment
+			if 90 < angle <= 180:
+				if ranges[i] < distance:
+					left_clear = False
+
+			if -180 <= angle < -90:
+				if ranges[i] < distance:
+					right_clear = False
+
+			if desired_angle_deg > min_turn_angle and not left_clear:
+				return 0
+			if desired_angle_deg  < -min_turn_angle and not right_clear:
+				return 0
+		
+		return desired_angle
+	
+	def calculate_velocity(self, gap_distance):
+		# dynamic velocity
+		# we can do dynamically based off of the gap distance (?)
+		# could also adjust based off of the angle we need to change
+		# could also adjust based off of the closest object
+
+		# if the gap is further than 3 meters, drive the max speed
+		if gap_distance > 3:
+			return self.max_vel
+
+		# if the gap is really close, drive the min velocity
+		if gap_distance < 0.5:
+			return self.min_vel
+		
+		# else, scale based on the distance
+		velocity = gap_distance/3.0 * self.max_vel
+		velocity = max(self.min_vel, min(self.max_vel, velocity))
+		print("velocity", velocity)
+
+		return velocity
+
+	def publish_drive(self, steering_angle, velocity):
+		# publish drive
+		cmd = AckermannDrive()
+		cmd.steering_angle = steering_angle
+		cmd.speed =  velocity
+		self.drive_pub.publish(cmd)
+
+	def visualize(self, scan_msg, processed_ranges, gap_idx, gap_dist):
+		# Publish processed scan
+		new_scan = LaserScan()
+		new_scan.header = scan_msg.header
+		new_scan.angle_min = scan_msg.angle_min
+		new_scan.angle_max = scan_msg.angle_max
+		new_scan.angle_increment = scan_msg.angle_increment
+		new_scan.time_increment = scan_msg.time_increment
+		new_scan.scan_time = scan_msg.scan_time
+		new_scan.range_min = scan_msg.range_min
+		new_scan.range_max = scan_msg.range_max
+		new_scan.ranges = processed_ranges
+		new_scan.intensities = scan_msg.intensities
+		self.targets.publish(new_scan)
+
+		# Publish target marker
+		arrow_marker = Marker()
+		arrow_marker.header.frame_id = "car_4_base_link"
+		arrow_marker.type = 2
+		arrow_marker.header.stamp = rospy.Time.now()
+		arrow_marker.id = 1
+
+		angle = scan_msg.angle_min + gap_idx * scan_msg.angle_increment
+		quat = quaternion_from_euler(0, 0, angle)
+		arrow_marker.pose.position.x = gap_dist * math.cos(angle)
+		arrow_marker.pose.position.y = gap_dist * math.sin(angle)
+		arrow_marker.scale.x = 0.2
+		arrow_marker.scale.y = 0.2
+		arrow_marker.scale.z = 0.2
+		arrow_marker.pose.orientation.x = quat[0]
+		arrow_marker.pose.orientation.y = quat[1]
+		arrow_marker.pose.orientation.z = quat[2]
+		arrow_marker.pose.orientation.w = quat[3]
+		arrow_marker.color.r = 0.0
+		arrow_marker.color.g = 1.0
+		arrow_marker.color.b = 0.0
+		arrow_marker.color.a = 1.0
+		self.targetPoint.publish(arrow_marker)
 
 
 if __name__ == '__main__':
 
 	rospy.init_node('ftg_controller', anonymous=True)
+	FTGController()
 	rospy.spin()
