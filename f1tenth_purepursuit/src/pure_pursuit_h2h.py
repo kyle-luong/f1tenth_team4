fde5d8dcc881a5ccfc3d585a4d89f6f23c0cc5f7
kyle-luong <kyleluong2004@gmail.com>
2025-12-11 13:58:48 -0500

    updated h2h

--- f1tenth_purepursuit/src/pure_pursuit_h2h.py
+++
@@ -1,6 +0,0 @@
-#TODO
-#copy pure pursuit
-#ideas;
-# prof mentioned path toggling between parallel lines
-# we need to worry about crashing into car infront of us like if something is infront of us we need to slow down alot
-# 
